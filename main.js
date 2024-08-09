f7c6405a475bd07645e81d0b6a0ae6c378bc9946
Shuxrat <shuxratturaev96@gmail.com>
2024-08-09 17:55:48 +0500

    script

---
+++ main.js
@@ -0,0 +1,29 @@
+var myName = prompt ('Введите своё имя')
+alert (myName)
+console.log(' Меня зовут ' + myName);
+
+var myAge = prompt ('Введите ваш возраст')
+alert (myAge)
+console.log(' Мне ' + myAge + ' лет ');
+
+var plus = prompt ('Решите пример 10+5=?')
+alert (plus)
+console.log(' Пример 1: 10 + 5 = ' + ( 10 + 5 ) + ' Ваш ответ ' + plus );
+
+var minus = prompt ('Решите пример 10-5=?')
+alert (minus)
+console.log(' Пример 2: 10 - 5 = ' + ( 10 - 5 ) + ' Ваш ответ ' + minus );
+
+var multiply = prompt ('Решите пример 10*5=?')
+alert (multiply)
+console.log(' Пример 1: 10 *5 = ' +  10 * 5  + ' Ваш ответ ' + multiply );
+
+var divide = prompt ('Решите пример 10/5=?')
+alert (divide)
+console.log(' Пример 1: 10 / 5 = ' +  10 / 5  + ' Ваш ответ ' + divide );
+
+var percent = prompt ('Решите пример 10%5=?')
+alert (percent)
+console.log(' Пример 1: 10 % 5 = ' + ( 10 % 5 ) + ' Ваш ответ ' + percent );
+
+alert ('Войдите в console')
